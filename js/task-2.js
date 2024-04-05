be8459bf536648457bf4b29be4737ffc3594cee7
Denys <denis.itsme.1@gmail.com>
2024-04-05 18:05:58 +0300

    add comment for task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -5,6 +5,7 @@ function formatMessage(message, maxLength) {
   } else {
     return message.slice(0, maxLength) + '...';
   }
+  // return message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
 }
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
 console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
